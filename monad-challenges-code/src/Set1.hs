20ac781b2d7b73dd64de8b7c209c09abd11a529b
rahulbhargav <p.rahulbhargav@outlook.com>
2020-11-22 17:59:20 +0530

    .

--- monad-challenges-code/src/Set1.hs
+++ monad-challenges-code/src/Set1.hs
@@ -21,29 +21,38 @@ randString l = (\x -> fst $ randLetter (mkSeed x)) `map` (nRands l)
 randString3 :: String
 randString3 = randString 3
 
-
-
-
 type Gen a = (a, Seed)
 
-generalA :: (a -> a) -> Gen a
-generalA genF = 
-    let (x, y) = rand (mkSeed 1)
+generalA :: (Integer -> b) -> Gen a -> Gen b
+generalA genF g = 
+    let (x, y) = rand (snd g)
+    in (genF x, y)
 
-randEven :: Gen Integer
-randEven = 
-    let (x, y) = rand (mkSeed 1)
-    in (x * 2, y)
+randEven :: Gen Integer -> Gen Integer
+randEven = generalA (2 *) 
 
-randOdd :: Gen Integer
-randOdd = 
-    let (x, y) = randEven (mkSeed 1)
-    in (x + 1, y)
+randOdd :: Gen Integer -> Gen Integer
+randOdd = generalA (\x -> (2 * x) + 1)
 
--- randTen :: Gen Integer -- the output of rand * 10
-randTen = 
-    let (x, y) = rand (mkSeed 1)
-    in (x * 10, y)
+randTen :: Gen Integer -> Gen Integer
+randTen = generalA (10 *)
+
+randChar :: Gen Char -> Gen Char
+randChar = generalA toLetter
+
+randNum :: Gen Integer -> Gen Integer
+randNum = generalA fromIntegral
+
+-- generalPair :: Gen a -> Gen b -> Gen (a,b)
+-- generalPair gA gB = 
+--     let (c,s1) = randChar gA
+--     in let (n, s2) = rand
+--         in ((c,n), s2)
+    -- (n,s2) = randNum (1, s1)
+    -- ((c,n), s2)
+
+-- randPair :: Gen (Char, Integer)
+-- randPair = generalA (\x -> x) randChar ('', mkSeed 1)
 
 
 
